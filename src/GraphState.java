3fc416494c233ec20dc753d689b518ecfb3967bc
sfleischer <fls@seas.upenn.edu>
2016-11-23 02:24:58 -0500

    Began to add OptionPanes

--- src/GraphState.java
+++ src/GraphState.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class GraphState {
 	private double STD;
@@ -36,12 +38,43 @@ public class GraphState {
 		population = 50;
 	}
 	
+	private static void generateWarning(String title, String message){
+		JOptionPane.showMessageDialog(new JFrame(),  
+				message, 
+				title,
+				JOptionPane.WARNING_MESSAGE);
+	}
+	
 	public void setSTD(double s)	{ STD = s; }
 	public void setMean(double m)	{ mean = m; }
 	public void setMedian(double m)	{ median = m; }
-	public void setMin(double m)	{ min = m; }
-	public void setMax(double m)	{ max = m; }
-	public void setPeople(int p)	{ people = p; }
+	
+	public void setMin(double m){ 
+		if(m < 0 || m > 100){
+			generateWarning("Min Warning", "Min should be between 0 and 100");
+		}
+		min = m > 100 ? 100 : m;
+		min = m < 0 ? 0 : min;
+	}
+	
+	public void setMax(double m){
+		if(m < 0 || m > 100){
+			generateWarning("Max Warning", "Max should be between 0 and 100");
+		}
+		max = m > 100 ? 100 : m;
+		max = m < 0 ? 0 : max;
+	}
+	
+	public void setPeople(int p){
+		if(p < 0){
+			generateWarning("People Warning", "Cannot have negative people");
+		} else if(p > 1000){
+			generateWarning("People Warning", "Cannot handle more than a 1000 people");
+		}
+		people = p > 1000 ? 1000 : p;
+		people = p < 0 ? 0 : people;
+	}
+	
 	public void setPersonal(double p){ personal = p; }
 	
 	public void setXSpacing(double s)	{ spacing = s; }
@@ -51,6 +84,9 @@ public class GraphState {
 	public void setGenerations(int g)   { generations = g; }
 	public void setMutation(double m)   { mutation = m; }
 	public void setThreshold(int t){
+		if(threshold < 0 || threshold > 1000){
+			generateWarning("Threshold Warning", "Threshold is between 0 and 1000");
+		}
 		threshold = t > 1000 ? 1000 : t; 
 		threshold = t < 0 ? 0 : threshold;
 	}
